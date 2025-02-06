c40f1b85bd1e1d124eb824872225726b2f1e9f29
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-06 12:51:31 +0100

    Created methods for replacing target with a replacement

--- src/main/java/commands/ReplaceTextCommand.java
+++ src/main/java/commands/ReplaceTextCommand.java
@@ -1,4 +1,25 @@
 package commands;
 
-public class ReplaceTextCommand {
+public class ReplaceTextCommand extends TextCommand{
+  private String target;
+  private String replacement;
+
+
+  public String replaceTextCommand(String text, String target, String replacement){
+    if(text.contains(target)){
+      return text.replace(target, replacement);
+    }else{
+      System.out.println("Target was not found in the given text");
+    }
+    return text;
+  }
+
+
+  public String getTarget(){
+    return target;
+  }
+
+  public String getReplacement(){
+    return replacement;
+  }
 }
