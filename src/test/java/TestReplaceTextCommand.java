ef1e19f582a11e6043bc79b44089dfa9ad5e1f26
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-06 13:12:42 +0100

    Added both negative and positive tests for replaceTextCommand

--- src/test/java/TestReplaceTextCommand.java
+++ src/test/java/TestReplaceTextCommand.java
@@ -1,2 +1,22 @@
+import commands.ReplaceTextCommand;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestReplaceTextCommand {
-}
+
+  @Test
+  public void testReplaceText() {
+    ReplaceTextCommand replace = new ReplaceTextCommand();
+    String result = replace.replaceTextCommand("This is a text", "text", "replaced text");
+    assertEquals("This is a replaced text", result);
+  }
+
+  @Test
+  public void testNoReplacement() {
+    ReplaceTextCommand replace = new ReplaceTextCommand();
+    assertThrows(IllegalArgumentException.class, () ->
+        replace.replaceTextCommand("This is a text", "fish", "fishes")
+    );
+    }
+  }
