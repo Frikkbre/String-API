b08e275c6f5b829f5850561a1f902112f3cec5d5
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 17:15:08 +0100

    Added class file and impelemted TextCommand interface aswell as wraps text

---
+++ src/main/java/commands/WrapTextCommand.java
@@ -0,0 +1,25 @@
+package commands;
+
+public class WrapTextCommand implements TextCommand {
+  String opening;
+  String end;
+
+  public WrapTextCommand(String opening, String end){
+    this.opening = opening;
+    this.end = end;
+  }
+
+  /**
+   * Wraps the given text with the opening and end strings
+   * @param text
+   * @return the wrapped text
+   */
+  @Override
+  public String execute(String text) {
+    if (text == null) {
+      throw new IllegalArgumentException("Text cannot be null");
+    } else {
+      return opening + text + end;
+    }
+  }
+}
