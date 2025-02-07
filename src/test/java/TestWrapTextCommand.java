e3fda0d5d225fb6019bfbb7cdb034c120ec3a41c
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 17:21:43 +0100

    Added positive and negative tests for WrapTextCommand class

--- src/test/java/TestWrapTextCommand.java
+++ src/test/java/TestWrapTextCommand.java
@@ -1,2 +1,24 @@
+import commands.ReplaceFirstTextCommand;
+import commands.ReplaceTextCommand;
+
+import commands.WrapTextCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestWrapTextCommand {
+  @Test
+  public void TestWrapTextCommand(){
+    WrapTextCommand wrap = new WrapTextCommand("Hei!","MVH xxx xxxx");
+    String result = wrap.execute("Dette er en jobb mail");
+    assertEquals("Hei! Dette er en jobb mail MVH xxx xxxx", result);
+  }
+
+
+  @Test
+  public void TestWrapTextCommandNegative(){
+    WrapTextCommand wrap = new WrapTextCommand("Hei!","MVH xxx xxxx");
+    assertThrows(IllegalArgumentException.class, () ->
+        wrap.execute("")
+    );
+  }
 }
