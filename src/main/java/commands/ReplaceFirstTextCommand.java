d32d3676843980ff0189e86b3f5cde4acedf773c
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 16:13:19 +0100

    Added class ReplaceFirstTextCommand

--- src/main/java/commands/ReplaceFirstTextCommand.java
+++ src/main/java/commands/ReplaceFirstTextCommand.java
@@ -1,4 +1,16 @@
 package commands;
 
-public class ReplaceFirstTextCommand {
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String replaceTextCommand(String text, String target, String replacement) {
+    if (text.contains(target)) {
+      return text.replaceFirst(target, replacement);
+    } else {
+      throw new IllegalArgumentException("Target was not found in the given text");
+    }
+  }
 }
