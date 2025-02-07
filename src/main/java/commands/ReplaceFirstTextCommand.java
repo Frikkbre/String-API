05eaa3348a67355110faa956aa5c0e8f6b2f5518
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 17:22:21 +0100

    Changed structure

--- src/main/java/commands/ReplaceFirstTextCommand.java
+++ src/main/java/commands/ReplaceFirstTextCommand.java
@@ -5,6 +5,13 @@ public class ReplaceFirstTextCommand extends ReplaceTextCommand {
     super(target, replacement);
   }
 
+  /**
+   * Replaces the first occurrence of the target string in the given text with the replacement string.
+   * @param text
+   * @param target
+   * @param replacement
+   * @return text with the first occurrence of the target string replaced with the replacement string
+   */
   @Override
   public String replaceTextCommand(String text, String target, String replacement) {
     if (text.contains(target)) {
