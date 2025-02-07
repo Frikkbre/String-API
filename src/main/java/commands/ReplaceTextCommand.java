40230550a0185c91cd4a5288c84e78fc61ccd9de
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 16:11:38 +0100

    Changed from extends to implements TextCommand interface

--- src/main/java/commands/ReplaceTextCommand.java
+++ src/main/java/commands/ReplaceTextCommand.java
@@ -1,9 +1,17 @@
 package commands;
 
-public class ReplaceTextCommand extends TextCommand{
-  private String target;
-  private String replacement;
+public class ReplaceTextCommand implements TextCommand{
+  private final String target;
+  private final String replacement;
 
+  public ReplaceTextCommand(String target, String replacement){
+    this.target = target;
+    this.replacement = replacement;
+  }
+
+  public String execute(String text){
+    return replaceTextCommand(text, target, replacement);
+  }
 
   public String replaceTextCommand(String text, String target, String replacement){
     if(text.contains(target)){
