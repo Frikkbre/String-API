a0560dccaf7d566354c3b7eaecfe0314b30e6efb
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 17:22:07 +0100

    Updated negative test

--- src/test/java/TestReplaceFirstTextCommand.java
+++ src/test/java/TestReplaceFirstTextCommand.java
@@ -1,3 +1,4 @@
+import commands.ReplaceFirstTextCommand;
 import commands.ReplaceTextCommand;
 
 import org.junit.jupiter.api.Test;
@@ -5,5 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestReplaceFirstTextCommand {
 
+  @Test
+  public void TestReplaceFirstText(){
+    ReplaceFirstTextCommand replace = new ReplaceFirstTextCommand("text","replaced");
+    String result = replace.execute("This is a text text but only the first text is replaced!");
+    assertEquals("This is a replaced text but only the first text is replaced!", result);
+  }
+
+  @Test
+  public void TestReplaceFirstTextNegative(){
+    ReplaceFirstTextCommand replace = new ReplaceFirstTextCommand("text","replaced");
+    assertThrows(IllegalArgumentException.class, () ->
+        replace.execute("This does not include the target word....")
+    );
+  }
 
 }
