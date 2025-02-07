014099d0e2592bf8403e28167b6cd81532cadbf2
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 16:20:31 +0100

    Changed method call to suit new structure in ReplaceTextCommand

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import commands.ReplaceTextCommand;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    ReplaceTextCommand replace = new ReplaceTextCommand("Frikk", "Aoife");
+    replace.execute("Hei jeg heter Frikk");
   }
 }
