313ac2c0a1d4c95cc699b724775bb62cae3f141c
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-07 16:22:19 +0100

    removed debug method from psvm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,5 @@ import commands.ReplaceTextCommand;
 public class Main {
 
   public static void main(String[] args) {
-    ReplaceTextCommand replace = new ReplaceTextCommand("Frikk", "Aoife");
-    replace.execute("Hei jeg heter Frikk");
   }
 }
