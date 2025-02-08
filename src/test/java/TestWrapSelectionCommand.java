346df17d5c56bddc5f5016de0713c0d88d09fcac
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-08 15:18:54 +0100

    Added working positive and negative tests for WrapSelectionTextCommand class

--- src/test/java/TestWrapSelectionCommand.java
+++ src/test/java/TestWrapSelectionCommand.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import commands.WrapSelectionTextCommand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestWrapSelectionCommand {
+    @Test
+    public void TestWrapSelectionCommand(){
+        WrapSelectionTextCommand wrap = new WrapSelectionTextCommand("this section","THIS SELECTION","IS WRAPPED");
+        String result = wrap.execute("This is a text and i shall wrap this section in this text");
+        assertEquals("This is a text and i shall wrap THIS SELECTION this section IS WRAPPED in this text", result);
+    }
+
+
+    @Test
+    public void TestWrapTextCommandNegative(){
+        WrapSelectionTextCommand wrap = new WrapSelectionTextCommand("this section","THIS SELECTION","IS WRAPPED");
+        assertThrows(IllegalArgumentException.class, () ->
+                wrap.execute("")
+        );
+    }
 }
