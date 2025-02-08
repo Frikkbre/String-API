b827f089b115a1c6a7fe0196f085ffc5d7ae2331
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-08 15:32:23 +0100

    Added positive and negative tests to WrapLinesTextCommand class

--- src/test/java/TestWrapLinesTextCommand.java
+++ src/test/java/TestWrapLinesTextCommand.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import commands.WrapLinesTextCommand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestWrapLinesTextCommand {
+    @Test
+    public void TestWrapLinesTextCommand(){
+        WrapLinesTextCommand wrap = new WrapLinesTextCommand("Start","End");
+        String result = wrap.execute("Test Line 1\nTest Line 2\nTest Line 3");
+        assertEquals("StartTest Line 1End\nStartTest Line 2End\nStartTest Line 3End", result);
+    }
+
+
+    @Test
+    public void TestWrapTextCommandNegative(){
+        WrapLinesTextCommand wrap = new WrapLinesTextCommand("Start","End");
+        assertThrows(IllegalArgumentException.class, () ->
+                wrap.execute("")
+        );
+    }
 }
