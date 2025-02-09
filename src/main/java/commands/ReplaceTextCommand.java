b1c7719f41f0fc1be76b54719c7e97f4096f48d3
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:06:05 +0100

    Added JavaDoc

--- src/main/java/commands/ReplaceTextCommand.java
+++ src/main/java/commands/ReplaceTextCommand.java
@@ -1,18 +1,34 @@
 package commands;
-
+/**
+ * ReplaceTextCommand
+ */
 public class ReplaceTextCommand implements TextCommand{
   private final String target;
   private final String replacement;
-
+/**
+ * Constructor for the ReplaceTextCommand
+ */
   public ReplaceTextCommand(String target, String replacement){
     this.target = target;
     this.replacement = replacement;
   }
 
+  /**
+   * Replaces the target string with the replacement string
+   * @param text
+   * @return
+   */
   public String execute(String text){
     return replaceTextCommand(text, target, replacement);
   }
 
+  /**
+   * Replaces the target string with the replacement string
+   * @param text
+   * @param target
+   * @param replacement
+   * @return
+   */
   public String replaceTextCommand(String text, String target, String replacement){
     if(text.contains(target)){
       return text.replace(target, replacement);
@@ -22,11 +38,18 @@ public class ReplaceTextCommand implements TextCommand{
     }
   }
 
-
+  /**
+   * Getter for the target string
+   * @return
+   */
   public String getTarget(){
     return target;
   }
 
+  /**
+   * Getter for the replacement string
+   * @return
+   */
   public String getReplacement(){
     return replacement;
   }
