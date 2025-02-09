d91ddee256dbff36848a132fd7ad09de8a140f79
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:04:41 +0100

    Added JavaDoc

--- src/main/java/commands/CapitalizeWordsTextCommand.java
+++ src/main/java/commands/CapitalizeWordsTextCommand.java
@@ -1,9 +1,17 @@
 package commands;
 
+/**
+ * CapitalizeWordsTextCommand
+ */
 public class CapitalizeWordsTextCommand implements TextCommand{
   public CapitalizeWordsTextCommand(){
   }
 
+  /**
+   * Capitalizes all the words in the given text
+   * @param text
+   * @return
+   */
   @Override
   public String execute(String text) {
     if(text.isBlank()){
