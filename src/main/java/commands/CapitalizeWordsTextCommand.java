3801bb44728ac8c336c898dfc87e193f8bc02565
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:03:39 +0100

    Added class to capitalize a text

--- src/main/java/commands/CapitalizeWordsTextCommand.java
+++ src/main/java/commands/CapitalizeWordsTextCommand.java
@@ -1,4 +1,14 @@
 package commands;
 
-public class CapitalizeWordsTextCommand {
+public class CapitalizeWordsTextCommand implements TextCommand{
+  public CapitalizeWordsTextCommand(){
+  }
+
+  @Override
+  public String execute(String text) {
+    if(text.isBlank()){
+      throw new IllegalArgumentException("Text cannot be empty");
+    }
+    return text.toUpperCase();
+  }
 }
