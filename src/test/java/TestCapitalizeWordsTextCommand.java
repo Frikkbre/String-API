e4ad6b2b182d7ba52f1a13cf9c926178ee7cd163
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:04:26 +0100

    Added javaDoc

--- src/test/java/TestCapitalizeWordsTextCommand.java
+++ src/test/java/TestCapitalizeWordsTextCommand.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Test for CapitalizeWordsTextCommand
+ */
 public class TestCapitalizeWordsTextCommand {
   @Test
   public void TestCapitalizeWordsTextCommand(){
