26cdbaff4af800e0127ad4ed01156c237164a530
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:22:12 +0100

    Added positive and negative tests for class CapitalizeSelectionTextCommand

--- src/test/java/TestCapitalizeSelectionTextCommand.java
+++ src/test/java/TestCapitalizeSelectionTextCommand.java
@@ -1,2 +1,24 @@
+import commands.CapitalizeSelectionTextCommand;
+
+import commands.ReplaceFirstTextCommand;
+import commands.ReplaceTextCommand;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCapitalizeSelectionTextCommand {
+  @Test
+  public void TestCapitalizeWordsTextCommand(){
+    CapitalizeSelectionTextCommand word = new CapitalizeSelectionTextCommand("this");
+    String result = word.execute("Capitalize this text");
+    assertEquals("Capitalize THIS text", result);
+  }
+
+  @Test
+  public void TestCapitalizeWordsTextCommandNegative(){
+    CapitalizeSelectionTextCommand word = new CapitalizeSelectionTextCommand("this");
+    assertThrows(IllegalArgumentException.class, () ->
+        word.execute("")
+    );
+  }
 }
