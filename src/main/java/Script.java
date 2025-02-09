3be8defdfeec66a9314d1849b197040cfb55847d
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:33:59 +0100

    Adde d a loop that runs the text through all the commands and returns the final result

--- src/main/java/Script.java
+++ src/main/java/Script.java
@@ -1,2 +1,27 @@
-public class Script {
+import commands.TextCommand;
+
+import java.util.List;
+
+/**
+ * Created by Frikk Brændsrød on 2/9/125.
+ * This class has a list of commands defined in its constructor
+ * The mehod execute runs the commands in the list
+ * the result from the first command is passed to the next command
+ * returns the result from the last command
+ */
+public class Script implements TextCommand {
+  private List<TextCommand> commands;
+
+  public Script(List<TextCommand> commands){
+    this.commands = commands;
+  }
+
+  @Override
+  public String execute(String text){
+    String result = text;
+    for(TextCommand command : commands){
+      result = command.execute(result);
+    }
+    return result;
+  }
 }
