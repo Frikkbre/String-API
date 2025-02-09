b3a19f9e4f273e554ec2005198e37839b086d9b6
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:16:00 +0100

    Added execute method

--- src/main/java/commands/CapitalizeWordsTextCommand.java
+++ src/main/java/commands/CapitalizeWordsTextCommand.java
@@ -1,4 +1,21 @@
 package commands;
 
-public class CapitalizeWordsTextCommand {
+public class CapitalizeWordsTextCommand extends CapitalizeTextCommand{
+  public CapitalizeWordsTextCommand(){
+  }
+
+  @Override
+  public String execute(String text) {
+    if (text.isBlank()) {
+      throw new IllegalArgumentException("Text cannot be empty");
+    }
+    String[] words = text.split(" ");
+    StringBuilder result = new StringBuilder();
+    for (String word : words) {
+      result.append(Character.toUpperCase(word.charAt(0)))
+          .append(word.substring(1).toLowerCase())
+          .append(" ");
+    }
+    return result.toString().trim();
+  }
 }
