a0eef2c28e708320ff12b21b38cdd90d8f877f55
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:21:45 +0100

    Added execute method and exception handling

--- src/main/java/commands/CapitalizeSelectionTextCommand.java
+++ src/main/java/commands/CapitalizeSelectionTextCommand.java
@@ -1,4 +1,26 @@
 package commands;
 
+/**
+ * CapitalizeSelectionTextCommand capitalizes a selection of the text defined by target variable
+ * and returns the result.
+ */
 public class CapitalizeSelectionTextCommand {
+  String target;
+
+  public CapitalizeSelectionTextCommand(String target){
+    this.target = target;
+  }
+
+  /**
+   * Capitalizes the target word in the given text
+   * @param text
+   * @return
+   */
+  public String execute(String text) {
+    if(text.isBlank()){
+      throw new IllegalArgumentException("Text cannot be empty");
+    }
+    return text.replace(target, target.toUpperCase());
+  }
+
 }
