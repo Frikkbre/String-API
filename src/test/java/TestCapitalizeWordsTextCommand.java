b46c0770ead4a22a847be126e9c01fe6dc53274f
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-09 14:16:26 +0100

    Added negative and positive tests for CapitalizeWordsTextCommand

--- src/test/java/TestCapitalizeWordsTextCommand.java
+++ src/test/java/TestCapitalizeWordsTextCommand.java
@@ -1,2 +1,24 @@
-public class TestCapitalizeWordsTextCommand {
+import commands.CapitalizeWordsTextCommand;
+import commands.ReplaceFirstTextCommand;
+import commands.ReplaceTextCommand;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCapitalizeWordsTextCommand{
+
+  @Test
+  public void TestCapitalizeWordsTextCommand(){
+    CapitalizeWordsTextCommand word = new CapitalizeWordsTextCommand();
+    String result = word.execute("capitalize each first letter");
+    assertEquals("Capitalize Each First Letter", result);
+  }
+
+  @Test
+  public void TestCapitalizeWordsTextCommandNegative(){
+    CapitalizeWordsTextCommand word = new CapitalizeWordsTextCommand();
+    assertThrows(IllegalArgumentException.class, () ->
+        word.execute("")
+    );
+  }
 }
